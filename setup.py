de3e02ca417c24c7a40e940750dfdb9e6a415e7c
Sriram Yeluri <yeluris@gmail.com>
2023-02-20 16:01:33 +0100

    initial helper setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sy-helper',
+    version='1.0.0',
+    packages=['sy-helper'],
+    url='https://github.com/sriram-yeluri/sy-helper.git',
+    license='MIT License',
+    author='sriramyeluri',
+    author_email='sriram.yeluri@outlook.com',
+    description='helper for http requests , loggin and other utilities for data analysis projects'
+)
